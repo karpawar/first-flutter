b550298e287ae81037ee180b62e84ac3e94321d1
karpawar <karpawar@gmail.com>
2020-08-22 02:05:29 +0530

    nav page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords()
+        theme: ThemeData(          // Add the 3 lines from here...
+          primaryColor: Colors.black,
+        ),
+        home: RandomWords()
     );
   }
 }
@@ -33,12 +36,46 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions() {
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
+    );
+  }
+
+Widget _buildSuggestions() {
     return ListView.builder(
         padding: EdgeInsets.all(16.0),
         itemBuilder: /*1*/ (context, i) {
@@ -74,4 +111,5 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
+
 }
